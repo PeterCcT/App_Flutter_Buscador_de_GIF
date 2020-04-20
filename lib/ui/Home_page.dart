75ff7c809d69bf561874be0633943a581c5b3f06
PeterCcT <pedrorodriguesfereira@gmail.com>
2020-04-20 15:23:19 -0300

    Adicionado compartilhamento de gifs

--- lib/ui/Home_page.dart
+++ lib/ui/Home_page.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:buscador_de_gif/ui/gif_pag.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -74,6 +75,7 @@ class _HomeState extends State<Home> {
               onSubmitted: (text) {
                 setState(() {
                   _search = text;
+                  _offset = 0;
                 });
               },
             ),
@@ -140,6 +142,10 @@ class _HomeState extends State<Home> {
                   builder: (context) => GifPage(snapshot.data["data"][index]),
                 ),
               );
+            },
+            onLongPress: () {
+              Share.share(snapshot.data["data"][index]["images"]["fixed_height"]
+                  ["url"]);
             },
           );
         else
--- lib/ui/gif_pag.dart
+++ lib/ui/gif_pag.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:share/share.dart';
 
 class GifPage extends StatelessWidget {
   final Map _dadosGif;
@@ -11,6 +12,14 @@ class GifPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(_dadosGif["title"]),
         backgroundColor: Colors.black,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.share),
+            onPressed: () {
+              Share.share(_dadosGif["images"]["fixed_height"]["url"]);
+            },
+          ),
+        ],
       ),
       backgroundColor: Colors.black,
       body: Center(
